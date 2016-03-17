c6a776809528e2f709fbe88d988638a550661cd1
Reece Hart <reecehart@gmail.com>
2016-03-17 11:39:59 -0700

    added support for blanks (_) in tiles

--- reece/sandbox/wordmatcher.py
+++ reece/sandbox/wordmatcher.py
@@ -2,7 +2,7 @@
 
 wget -nd http://iweb.dl.sourceforge.net/project/wordlist/speller/2016.01.19/hunspell-en_US-2016.01.19.zip
 unzip hunspell-en_US-2016.01.19.zip
-unmunch en_US.dic en_US.aff | perl -ne 'print unless m/\d|^[A-Z]|\x27s$|^[A-Z]+s?$|^\w$/' | sort -u | gzip >|words.txt.gz
+unmunch en_US.dic en_US.aff | perl -ne 'print unless m/\d|^[A-Z]|\x27|^[A-Z]+s?$|^\w$/' | sort -u | gzip >|words.txt.gz
 
 """
 
@@ -10,6 +10,41 @@ import collections
 import gzip
 import itertools
 import re
+
+
+def _sdiff(s1, s2):
+    """return symmetric diff of strings s1 and s2, as a tuple of (letters
+    unique to s1, letters common to both, letters unique to s2.
+
+    s1 and s2 must be strings with letters in sorted order
+
+    """
+
+    if s1 == s2:
+        return ('', s1, '')
+
+    i1 = i2 = 0
+    u1 = u2 = i = ""
+
+    while i1 < len(s1) and i2 < len(s2):
+        if s1[i1] == s2[i2]:
+            i += s1[i1]
+            i1 += 1
+            i2 += 1
+        elif s1[i1] <= s2[i2]:
+            u1 += s1[i1]
+            i1 += 1
+        elif s2[i2] <= s1[i1]:
+            u2 += s2[i2]
+            i2 += 1
+        else:
+            assert False, "Shouldn't be here"
+
+    u1 += s1[i1:]
+    u2 += s2[i2:]
+
+    return (u1, i, u2)
+
 
 class WordKey(str):
     """class representing a canonicalized form ("key") of the letters of a
@@ -38,8 +73,16 @@ class WordKey(str):
         return all(wc[l] <= uc[l] for l in wc)
 
 
+    def diff(w, u):
+        """returns a tuple of three strings, representing letters unique to w,
+        common to both, and unique to u"""
+        assert isinstance(w, WordKey) and isinstance(u, WordKey), "expected two WordKey instances"
+        return _sdiff(w, u)
+
 
 class WordMatcher(object):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
     def __init__(self, fn):
         words = [line.decode("UTF-8").strip() for line in gzip.open(fn)]
         key_word = [(WordKey(w), w) for w in words]
@@ -59,21 +102,38 @@ class WordMatcher(object):
         s_wk = WordKey(s)
         subkeys = [k for k in self._key_wordlist_map.keys() if k.spellable_with(s_wk)]
         return (w
-                for w in itertools.chain.from_iterable(self._key_wordlist_map[k] for k in subkeys)
-                if w != s)
+                for w in itertools.chain.from_iterable(self._key_wordlist_map[k] for k in subkeys))
 
-    def find_subwords_for_pattern(self, letters, pattern, min_length=3, must_use=""):
-        pletters = "".join(re.findall('\w', pattern))
-        return (w
-                for w in self.find_subwords(letters + pletters)
-                if (len(w) >= min_length and
-                    re.search(pattern, w) and
-                    (not must_use or all(l in w for l in must_use))))
+    # consider returning flat structure like {blanks, word}
+    # return in desc length order?
+    def find_subwords_for_pattern(self, tiles, pattern):
+        t_letters = tiles.replace('_', '')
+        n_blanks = len(tiles) - len(t_letters)
+        p_letters = "".join(re.findall('\w', pattern))
+        avail_letters = t_letters + p_letters
+        al_wk = WordKey(avail_letters)
+        returned = set()
 
+        wkeys = sorted(self._key_wordlist_map.keys(), key = lambda e: len(e), reverse = True)
 
-    def find_options(self, letters, patterns, min_length=3, must_use=""):
+        blank_key_pairs = [(b, k)
+                           for k, b in ((k, k.diff(al_wk)[0]) for k in wkeys)
+                           if len(b) <= n_blanks]
+        blank_key_pairs.sort(key=lambda bk: (len(bk[0]), bk[0]))
+
+        for b, bk_i in itertools.groupby(blank_key_pairs, key=lambda bk: (len(bk[0]), bk[0])):
+            words = [w
+                     for w in itertools.chain.from_iterable(self._key_wordlist_map[bk[1]] for bk in bk_i)
+                     if re.search(pattern, w)]
+            new_words = set(words) - returned
+            if len(new_words) > 0:
+                returned.update(new_words)
+                yield {'blanks': b, 'words': sorted(new_words, key=len, reverse=True)}
+
+    def find_options(self, tiles, patterns, min_length=2, must_use=""):
         for p in patterns:
-            yield (p, list(self.find_subwords_for_pattern(letters=letters, pattern=p, min_length=min_length, must_use=must_use)))
+            yield (p, list(self.find_subwords_for_pattern(
+                tiles=tiles, pattern=p)))
 
 if __name__ == "__main__":
     wm = WordMatcher("reece/sandbox/_data/wordlists/words.txt.gz")
